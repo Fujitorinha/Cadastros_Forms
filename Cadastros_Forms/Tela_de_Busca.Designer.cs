efb4c06201e44804261501c0f91cf22f6017dd80
Gabs <gabrielhp0812@gmail.com>
2023-06-19 20:19:02 -0300

    AAAAA

--- Cadastros_Forms/Tela_de_Busca.Designer.cs
+++ Cadastros_Forms/Tela_de_Busca.Designer.cs
@@ -29,49 +29,13 @@ namespace Cadastros_Forms
         /// </summary>
         private void InitializeComponent()
         {
-            this.button1 = new System.Windows.Forms.Button();
-            this.button2 = new System.Windows.Forms.Button();
-            this.button3 = new System.Windows.Forms.Button();
             this.SuspendLayout();
-            // 
-            // button1
-            // 
-            this.button1.Location = new System.Drawing.Point(12, 12);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(165, 63);
-            this.button1.TabIndex = 1;
-            this.button1.Text = "Para Clientes";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.Call_TelaClientes);
-            // 
-            // button2
-            // 
-            this.button2.Location = new System.Drawing.Point(208, 12);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(165, 63);
-            this.button2.TabIndex = 2;
-            this.button2.Text = "Para Funcionarios";
-            this.button2.UseVisualStyleBackColor = true;
-            this.button2.Click += new System.EventHandler(this.Call_TelaFuncionarios);
-            // 
-            // button3
-            // 
-            this.button3.Location = new System.Drawing.Point(404, 12);
-            this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(165, 63);
-            this.button3.TabIndex = 3;
-            this.button3.Text = "Para Produtos";
-            this.button3.UseVisualStyleBackColor = true;
-            this.button3.Click += new System.EventHandler(this.Call_TelaProdutos);
             // 
             // Tela_de_Busca
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(581, 83);
-            this.Controls.Add(this.button3);
-            this.Controls.Add(this.button2);
-            this.Controls.Add(this.button1);
+            this.ClientSize = new System.Drawing.Size(817, 459);
             this.Name = "Tela_de_Busca";
             this.Text = "Tela de Busca";
             this.ResumeLayout(false);
@@ -79,9 +43,5 @@ namespace Cadastros_Forms
         }
 
         #endregion
-
-        private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.Button button2;
-        private System.Windows.Forms.Button button3;
     }
 }
--- Cadastros_Forms/Tela_de_Busca.cs
+++ Cadastros_Forms/Tela_de_Busca.cs
@@ -16,27 +16,5 @@ namespace Cadastros_Forms
         {
             InitializeComponent();
         }
-
-        private void Call_TelaClientes(object sender, EventArgs e)
-        {
-            Pesquisa_Cliente Pesquisa_Cliente = new Pesquisa_Cliente();
-            Pesquisa_Cliente.Show();
-            this.Close();
-
-        }
-
-        private void Call_TelaFuncionarios(object sender, EventArgs e)
-        {
-            Pesquisa_Funcionarío Pesquisa_Funcionaro = new Pesquisa_Funcionarío();
-            Pesquisa_Funcionaro.Show();
-            this.Close();
-        }
-
-        private void Call_TelaProdutos(object sender, EventArgs e)
-        {
-            Pesquisa_Produto Pesquisa_Produto = new Pesquisa_Produto();
-            Pesquisa_Produto.Show();
-            this.Close();
-        }
     }
 }
